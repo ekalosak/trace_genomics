2a26d9707739fbbaa22041843b9cf27a508872a4
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2018-08-18 09:41:00 -0700

    opening data files

---
+++ explore.R
@@ -0,0 +1,12 @@
+# Author: Eric Kalosa-Kenyon
+#
+# Explore the metadata and otu data
+
+mdfn = "data_challenge-metadata.csv"
+ohfn = "data_challenge-otu_hits.csv"
+
+mddf = read.csv(mdfn)
+
+# library(neuralnet)
+# library(randomForest)
+# library(lmer)
